7089d4d4cc67e33281485f46944816f9c7ea8a87
stamp <stamp@jhRens-Mac-mini.local>
2022-12-07 15:55:03 +0900

    paste project

---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+import Document, {Html, Head, Main, NextScript} from "next/document"
+
+class MyDocument extends Document {
+    render(){
+        return(
+            <Html>
+                <Head>
+                    <link rel="manifest" href="/manifest.json" />
+                    <link rel="apple-touch-icon" href="/icon.png" />
+                    <meta name="theme-color" content="#fff"/>
+                </Head>
+                <body>
+                    <Main></Main>
+                    <NextScript></NextScript>
+                </body>
+            </Html>
+        )
+    }
+}
+export default MyDocument;
